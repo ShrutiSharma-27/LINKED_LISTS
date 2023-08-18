11105eb4c7f0f456c4e5550b1f94be3c0a57deec
Shruti Sharma <53565103+ShrutiSharma-27@users.noreply.github.com>
2023-08-18 23:09:04 +0530

    Update RemoveFirst_from_LL.java

--- RemoveFirst_from_LL.java
+++ RemoveFirst_from_LL.java
@@ -34,7 +34,7 @@ class LinkedList{
         return size;
     }
 
-    // HERE are the mention functions :
+    // HERE is the mentioned function :
     void removeFirst(){
         if(size==0) System.out.println("Linked list is empty");
         else if (size==1) {
