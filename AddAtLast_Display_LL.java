5e073e1bd98c65d2f0c33be434212519e26fd95a
Shruti Sharma <53565103+ShrutiSharma-27@users.noreply.github.com>
2023-08-18 16:26:08 +0530

    Create AddAtLast_Display_LL.java

---
+++ AddAtLast_Display_LL.java
@@ -0,0 +1,55 @@
+class Node{  //Node object points to the address where it has stored that particular node.
+    int data;
+    Node next;
+}
+
+class LinkedList{
+    Node head;
+    Node tail;
+    int size;
+
+    void addLast(int value){
+        Node temp=new Node();
+        temp.data=value;
+        temp.next=null;
+        if (size==0){
+            head = tail = temp;
+        }
+        else{
+            tail.next=temp ;
+            tail=temp;
+        }
+        size++;
+    }
+
+    void displayLinkedList(){
+        Node temp = head;
+        while (temp != null){
+            System.out.println(temp.data);
+            temp=temp.next;
+        }
+    }
+
+    int getSize(){
+        return size;
+    }
+}
+
+public class AddAtLast_Display_LL {
+    public static void main(String[] args){
+        LinkedList list = new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(70);
+
+        list.displayLinkedList();
+        System.out.println(list.getSize());
+    }
+}
+
+//Output :
+
+// 10
+// 20
+// 70
+// 3
